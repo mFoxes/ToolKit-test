d0a60df2eccefb5aabbb8ebc5e5afb598ec8dedd
Artem Evdokimo <drega2010@gmail.com>
2024-07-01 17:40:08 +0300

    init router and store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
