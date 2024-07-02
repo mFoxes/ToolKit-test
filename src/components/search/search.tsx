63092da2111f94e055604e200343d923c89fd71b
Artem Evdokimo <drega2010@gmail.com>
2024-07-02 18:20:48 +0300

    styling search

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,6 +1,7 @@
 import { Formik } from 'formik';
 import React, { ChangeEventHandler, useState } from 'react';
 import styled from 'styled-components';
+import { ReactComponent as SearchSvg } from '../../assets/svg/search.svg';
 
 interface SearchProps {
     onSubmit?: (value: string) => void;
@@ -14,14 +15,15 @@ export const Search = ({ onSubmit, ...props }: SearchProps) => {
     };
 
     const handleClick = () => {
-        console.log('value', value);
         onSubmit && onSubmit(value);
     };
 
     return (
         <Container>
-            <input value={value} onChange={handleChange} />
-            <button onClick={handleClick}></button>
+            <StyledInput value={value} onChange={handleChange} placeholder="Поиск..." />
+            <StyledButton onClick={handleClick}>
+                <SearchSvg />
+            </StyledButton>
         </Container>
     );
 };
@@ -30,4 +32,32 @@ const Container = styled.div`
     width: 100%;
     display: flex;
     justify-content: center;
+`;
+
+const StyledInput = styled.input`
+    padding: 4px 0 4px 8px;
+    border: 2px solid ${(props) => props.theme.colors.darkGrey};
+    border-right: none;
+    border-radius: 8px 0 0 8px;
+    outline: none;
+
+    font-size: 18px;
+`;
+
+const StyledButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${(props) => props.theme.colors.white};
+    border: 2px solid ${(props) => props.theme.colors.darkGrey};
+    border-radius: 0 8px 8px 0;
+
+    cursor: pointer;
+
+    padding: 4px 8px;
+    svg {
+        height: 24px;
+        width: 24px;
+    }
 `;
